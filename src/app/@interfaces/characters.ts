c505d1b7ab470a120437c7d3913e83cdf4ddf675
Anibal Villalva <fav1981@gmail.com>
2023-02-03 00:18:42 -0300

    add cards

---
+++ src/app/@interfaces/characters.ts
@@ -0,0 +1,11 @@
+export interface ICharacters {
+  id?: string;
+  name?: string;
+  actor?: string;
+  description?: string;
+  photo?: string;
+  total_episodes?: string;
+  votes?: string;
+  url?: string;
+}
+
